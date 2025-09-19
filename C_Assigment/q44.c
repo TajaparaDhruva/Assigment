974ad48c3edc45f4cb12164ca51762a5396c533b
DhruvaTajapara <dhruvatajapara@gmail.com>
2025-09-20 00:55:32 +0530

    C_Assignment

---
+++ C_Assigment/q44.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter n:");
+    scanf("%d",&n);
+    for(int i=n;i>=1;i--){
+        for(int j=1;j<=n;j++){
+           if(i==3 || j==3){
+            printf("*");
+           }
+           else{
+            printf(" ");
+           }
+        }
+        printf("\n");
+    }
+    return 0;
+}
